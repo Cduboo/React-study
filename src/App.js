41a4b2d5a730ff09553349d844ca8ad72188cb97
Cduboo <cheogn97@gmail.com>
2023-07-01 22:52:59 +0900

    props를 활용한 상품 목록 component 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {Button, Navbar, Nav, Container } from 'react-bootstrap';
+import data from './data.js';
 
 function App() {
+
+  let [item, setItem] = useState(data);
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">SHOP</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      {/* 상품 출력부 */}
+      <div className="container">
+        <div className="row">
+          {
+            item.map(function(item, i) {
+              return (
+                <Card key={item.id} title={item.title} content={item.content} price={item.price} index={i+1}></Card>
+              )
+            })
+          }
+        </div>
+
+        
+      </div>
     </div>
+
   );
+
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      {/* <div style={ {backgroundImage:`url("./img/item${props.index}.jpg")`} }>1</div> */}
+      <img src={`http://localhost:3000/item${props.index}.jpg`} width="100%"></img>
+      <h4>{props.title}</h4>
+      <p>{props.content}</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+let data = [
+  {
+    id : 0,
+    title : "markus-spiske",
+    content : "Lorem ipsum dolor sit",
+    price : 800000
+  },
+
+  {
+    id : 1,
+    title : "mcgill-library",
+    content : " amet consectetur adipisicing elit.",
+    price : 1200000
+  },
+
+  {
+    id : 2,
+    title : "pine-watt-3",
+    content : " Tenetur quasi enim dicta ipsa debitis",
+    price : 5000000
+  }
+];
+export default data;
