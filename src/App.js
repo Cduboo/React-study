943b29d53eb4aa6d3d010ebd108b13da9785b521
Cduboo <cheogn97@gmail.com>
2023-07-02 01:56:17 +0900

    react-router-dom을 활용한 Detail component 구현
    서브경로와 페이지 분리, 경로 이동, 파라미터값 활용한 상세페이지 분리 학습

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import {Button, Navbar, Nav, Container } from 'react-bootstrap';
+import { Button, Navbar, Nav, Container } from 'react-bootstrap';
 import data from './data.js';
-
+import Detail from './components/Detail.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 function App() {
 
   let [item, setItem] = useState(data);
+  let navigate = useNavigate();
+
   return (
 
     <div className="App">
+
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">SHOP</Navbar.Brand>
+          <Navbar.Brand onClick={ () => {navigate('/')} }>SHOP</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={ () => {navigate('/')} }>Home</Nav.Link>
+            <Nav.Link onClick={ ()=> {navigate('/detail')} }>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
-      <div className="main-bg"></div>
-      {/* 상품 출력부 */}
-      <div className="container">
-        <div className="row">
-          {
-            item.map(function(item, i) {
-              return (
-                <Card key={item.id} title={item.title} content={item.content} price={item.price} index={i+1}></Card>
-              )
-            })
-          }
-        </div>
-
-        
-      </div>
+      
+      <Routes>
+        <Route path="/" element={
+          <>
+          <div className="main-bg"></div>
+          {/* 상품 출력부 */}
+          <div className="container">
+            <div className="row">
+              {
+                item.map(function(item, i) {
+                  return (
+                    <Card navigate={navigate} key={item.id} title={item.title} content={item.content} price={item.price} index={i}></Card>
+                  )
+                })
+              }
+            </div>
+          </div>
+          </>
+        } />
+        <Route path="/detail/:id" element={<Detail item={item} />} />
+        <Route path="/about" element={<About></About>}>
+          <Route path="member" element={<div>member</div>}></Route>
+          <Route path="location" element={<div>location</div>}></Route>
+        </Route>
+        <Route path="/event" element={<><h4>오늘의 이벤트</h4><Outlet></Outlet></>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route path="two" element={<div>생일기념 구폰받기</div>}></Route>
+        </Route>
+        <Route path="*" element={<div>잘못된 요청입니다.</div>} />
+      </Routes>
+  
     </div>
 
   );
@@ -43,7 +62,7 @@ function App() {
 
 function Card(props) {
   return (
-    <div className="col-md-4">
+    <div className="col-md-4"onClick={ () => {props.navigate(`/detail/${props.index}`)} }>
       {/* <div style={ {backgroundImage:`url("./img/item${props.index}.jpg")`} }>1</div> */}
       <img src={`http://localhost:3000/item${props.index}.jpg`} width="100%"></img>
       <h4>{props.title}</h4>
@@ -52,4 +71,12 @@ function Card(props) {
   )
 }
 
+function About() {
+  return( 
+    <div>
+      About
+      <Outlet></Outlet>
+    </div>
+  )
+}
 export default App;
---
+++ src/components/Detail.js
@@ -0,0 +1,24 @@
+import { useParams } from 'react-router-dom';
+
+function Detail(props) {
+  let {id} = useParams();
+  let item = props.item.find( e => e.id == id);
+  console.log(id)
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={`http://localhost:3000/item${id}.jpg`} width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{item.title}</h4>
+          <p>{item.content}</p>
+          <p>{item.price}원</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Detail;
